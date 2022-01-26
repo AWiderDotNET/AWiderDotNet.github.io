71e9f54c71046ec124b11a3ad10e4b599cb38403
Sean Killeen <SeanKilleen@gmail.com>
2022-01-26 07:44:07 -0500

    initial statiq hello world

---
+++ AWiderDotNet.Statiq/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using Statiq.App;
+using Statiq.Web;
+
+namespace MySite
+{
+  public class Program
+  {
+    public static async Task<int> Main(string[] args) =>
+      await Bootstrapper
+        .Factory
+        .CreateWeb(args)
+        .RunAsync();
+  }
+}
